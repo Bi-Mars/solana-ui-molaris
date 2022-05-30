b2829e858df46baa8eb9603cb366c38609a75381
Mars <mars@Bimarshs-MacBook-Pro.local>
2022-05-30 15:36:19 -0500

    Front-end add using Molaris and Next.js to connect with Phantom wallet and get wallet information.

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,25 +1,80 @@
-export default function Dashboard() {
-    return(
+import { useEffect, useState } from "react"
+import { useMoralisSolanaApi } from "react-moralis"
+
+export default function Dashboard({ logout, user }) {
+    let walletAddress = user.get('solAddress')
+    let SolanaAPI = useMoralisSolanaApi()
+    let [solanaBalance, setSolanaBalance] = useState('')
+    let [splBalance, setSplBalance] = useState([{}])
+    let [nftsBalance, setNftsBalance] = useState([{}])
+    let [isLoading, setIsLoading] = useState(true)
+    //must be synchronous
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                let result = await SolanaAPI.account.balance({
+                    network: 'devnet',
+                    address: walletAddress
+                })
+                setSolanaBalance(result.solana)
+            } catch (error) {
+                console.log(error)
+            }
+
+            try {
+                let result = await SolanaAPI.account.getSPL({
+                    network: 'devnet',
+                    address: walletAddress
+                })
+                setSplBalance(result)
+            } catch (error) {
+                console.log(error)
+            }
+            try {
+                let result = await SolanaAPI.account.getNFTs({
+                    network: 'devnet',
+                    address: walletAddress
+                })
+                setNftsBalance(result)
+            } catch (error) {
+                console.log(error)
+            }
+            setIsLoading(false)
+        }
+        fetchData()
+    }, [])
+
+    return (
         <div className="w-screen h-screen flex flex-col items-center justify-center py-10 px-4 bg-black overflow-auto">
-            <button className="text-white self-end">logout</button>
+            <button onClick={logout} className="text-white self-end">logout</button>
             <p className="text-white font-bold text-xl md:text-3xl">wallet address</p>
-            <p className="text-white mt-2 mb-8 text-[0.6rem] md:text-lg">F1o6ykG8yM4kWAg2gCszxjkREd5S7JJnhetCqZszyoSr</p>
+            <p className="text-white mt-2 mb-8 text-[0.6rem] md:text-lg">{walletAddress}</p>
             <div className="w-full h-full max-w-4xl grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
                 <div className="bg-pink rounded-2xl drop-shadow-md px-2 py-2 md:px-4 md:py-4 md:text-lg">
                     <p className="text-2xl md:text-4xl">solana balance</p>
-                    <p className="mt-4 md:mt-10 text-3xl md:text-6xl">1.2356 <span>SOL</span></p>
+                    {
+                        !isLoading &&
+                        <p className="mt-4 md:mt-10 text-3xl md:text-6xl">{solanaBalance.slice(0, 6)} <span>SOL</span></p>
+                    }
                 </div>
                 <div className="bg-green rounded-2xl drop-shadow-md px-2 py-2 md:px-4 md:py-4 md:text-lg">
-                    <p className="text-2xl md:text-4xl">spl tokens 2</p>
+                    <p className="text-2xl md:text-4xl">spl tokens {!isLoading ? splBalance.length : ''}</p>
                     <ul className="list-disc ml-8 mt-4 md:mt-10 text-md md:text-lg">
-                        <li>Gh9ZwEm... {''} 1000</li>
-                        <li>Gh9ZwEm... {''} 1000</li>
+                        {
+                            splBalance.length > 0  && !isLoading && splBalance.map((spl, i) => (
+                                <li key={i}>{spl.mint?.slice(0, 12)}... {''} {spl.amount}</li>
+                            ))
+                        }
                     </ul>
                 </div>
                 <div className="bg-cyan md:col-span-2 rounded-2xl drop-shadow-md px-2 py-2 md:px-4 md:py-4 md:text-lg">
-                    <p className="text-2xl md:text-4xl">nfts 1</p>
+                    <p className="text-2xl md:text-4xl">nfts {!isLoading ? nftsBalance.length : ''}</p>
                     <ul className="list-disc px-4 mt-4 md:mt-10 text-md md:text-lg">
-                        <li>Gh9ZwEmwe8rewrew...</li>
+                        {
+                            nftsBalance.length > 0   && !isLoading && nftsBalance.map((nft, i) => (
+                                <li className="text-ellipsis overflow-hidden" key={i}>{nft.mint} </li>
+                            ))
+                        }
                     </ul>
                 </div>
             </div>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
+import { useMoralis } from "react-moralis";
 import Auth from "../components/Auth";
 import Dashboard from '../components/Dashboard'
 
 export default function Home() {
-  return (
-    <>
-      {/* <Auth /> */}
-      <Dashboard />
-    </>
-  )
+  const {
+    isAuthenticated,
+    authenticate,
+    logout,
+    user
+  } = useMoralis()
+
+  if (!isAuthenticated) {
+    return (<Auth authenticate={authenticate}/>)
+  }
+  return (<Dashboard logout={logout} user ={user}/>)
 }
